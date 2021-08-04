df6212197197d840d12a07c1bf7daf5b7f2c57b2
IvanGu <gusagusa@gmail.com>
2021-08-04 08:38:57 +0300

    Добавьте файлы проекта.

---
+++ СollatzРypothesisApp/CollatzChecker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace СollatzРypothesisApp
+{
+    class CollatzChecker
+    {
+        const int MAX_CHECK_STREAK = 5_000_000;
+        ulong maxCheckedNumber = 1;
+        SortedSet<ulong> checkedNumbers = new SortedSet<ulong>();
+        public ulong MaxCheckedNumber { get { return maxCheckedNumber; } }
+
+        public ulong[] CheckedNumbers { get { return checkedNumbers.ToArray(); } }
+
+
+
+        public CollatzChecker()
+        {
+            checkedNumbers.Add(1);
+        }
+
+        public void SetMaxCheckedNumber()
+        {
+            ulong counter = maxCheckedNumber + 1;
+            while (checkedNumbers.Contains(counter))
+                counter++;
+            maxCheckedNumber = counter - 1;
+            checkedNumbers.RemoveWhere(i => i <= maxCheckedNumber);
+        }
+
+        public void CheckNumber(ulong num)
+        {
+            int counter = 0;
+            var nums = new SortedSet<ulong>();
+            while (counter < MAX_CHECK_STREAK)
+            {
+                if (checkedNumbers.Contains(num) || num <= maxCheckedNumber)
+                {
+                    if (nums.Count > 0)
+                        checkedNumbers.UnionWith(nums);
+                    return;
+                }
+                nums.Add(num);
+                checked
+                {
+                    if (num % 2 == 0)
+                        num /= 2;
+                    else
+                        num = 3 * num + 1;
+                }
+                counter++;
+            }
+            throw new ArgumentException("Not a 1");
+        }
+    }
+}
---
+++ СollatzРypothesisApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace СollatzРypothesisApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new string('=', 20));
+            var cc = new CollatzChecker();
+            for (int epoch = 0; epoch < 9; epoch++)
+            {
+                for (ulong counter = cc.MaxCheckedNumber + 1; counter < cc.MaxCheckedNumber + 1_000_001; counter++)
+                    cc.CheckNumber(counter);
+                cc.SetMaxCheckedNumber();
+                Console.WriteLine($"epoch={epoch} maxNum={cc.MaxCheckedNumber} array ={cc.CheckedNumbers.Length}");
+            }
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
