6fbdd307e51a10e65870b1c36e38ca742522150a
IvanGu <gusagusa@gmail.com>
2021-08-04 15:46:31 +0300

    formatting

--- СollatzРypothesisApp/Program.cs
+++ СollatzРypothesisApp/Program.cs
@@ -4,18 +4,19 @@ namespace СollatzРypothesisApp
 {
     class Program
     {
+        const string CONSOLE_FORMAT_LONG="### ### ### ### ### ### ### ##0";
         static void Main(string[] args)
         {
             Console.WriteLine("Start " + new string('=', 20));
 
             var cc = CollatzChecker.GetInstanse();
 
-            for (int epoch = 0; epoch < 3; epoch++)
+            for (int epoch = 0; epoch < 10; epoch++)
             {
                 for (ulong counter = cc.MaxCheckedNumber + 1; counter < cc.MaxCheckedNumber + 1_000_001; counter++)
                     cc.CheckNumber(counter);
                 cc.DoEndEpoch();
-                Console.WriteLine($"epoch={epoch} maxNum={cc.MaxCheckedNumber} array ={cc.CheckedNumbers.Length}");
+                Console.WriteLine($"epoch=[{epoch}] maxCheckedNum=[{cc.MaxCheckedNumber.ToString(CONSOLE_FORMAT_LONG).Trim()}] array =[{cc.CheckedNumbers.Length.ToString(CONSOLE_FORMAT_LONG).Trim()}]");
             }
             Console.WriteLine("Done " + new string('=', 20));
         }
